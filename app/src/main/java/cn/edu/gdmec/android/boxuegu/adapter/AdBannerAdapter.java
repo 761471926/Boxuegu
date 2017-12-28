b4fb82f50cd6a7a591264012b7d273b758c9a408
761471926 <761471926@qq.com>
2017-12-28 09:46:40 +0800

    4.8

---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/adapter/AdBannerAdapter.java
@@ -0,0 +1,25 @@
+package cn.edu.gdmec.android.boxuegu.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by student on 17/12/28.
+ */
+
+public class AdBannerAdapter extends FragmentStatePagerAdapter {
+    public AdBannerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
